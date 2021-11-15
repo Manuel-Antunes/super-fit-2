8cf5e26691653d73dac203394900f007ed53faef
Manuel-Antunes <nerdbroz2@gmail.com>
2021-11-14 23:41:55 -0300

    chore: update user structure

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->float('wheight');
+            $table->string('birthDate');
+            $table->string('physics');
+            $table->enum('gender', ['m', 'f']);
             $table->rememberToken();
             $table->timestamps();
         });

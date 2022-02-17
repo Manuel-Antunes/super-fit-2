a9f88cc997dc02879253a6869eb9ecd5ac4f231f
Manuel-Antunes <nerdbroz2@gmail.com>
2022-02-17 02:54:26 -0300

    feat: all exercises routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExercisesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +21,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::resource('exercises', ExercisesController::class)->middleware(['auth']);
 
 require __DIR__ . '/auth.php';

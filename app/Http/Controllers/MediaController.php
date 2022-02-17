b6cedb97d10d4401f1d6d011bb20c8236cbd8c47
Manuel-Antunes <nerdbroz2@gmail.com>
2022-02-17 01:31:15 -0300

    feat: media model structure

---
+++ app/Http/Controllers/MediaController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Media;
+use Illuminate\Http\Request;
+
+class MediaController extends Controller
+{
+    public function store(Request $request)
+    {
+        $uploaded = [];
+
+        $files = $request->allFiles();
+        foreach ($files['files'] as $file) {
+            $nameFile = null;
+            $name = uniqid(date('HisYmd'));
+            $extension = $file->extension();
+            $nameFile = "{$name}.{$extension}";
+            // Retorna mime type do arquivo (Exemplo image/png)
+            $type = $file->getMimeType();
+
+            // Retorna o nome original do arquivo
+            $f = $file->getClientOriginalName();
+
+
+            $file->storeAs('uploads', $nameFile);
+            $media = Media::create([
+                'name' => $f,
+                'file' => $name,
+                'type' => $type,
+                'subtype' => $extension,
+            ]);
+            $uploaded[] = $media;
+        }
+        return $uploaded;
+    }
+}
---
+++ database/migrations/2022_02_17_035925_create_media_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->string('file')->nullable(false);
+            $table->string('type')->nullable(false);
+            $table->string('subtype')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+}

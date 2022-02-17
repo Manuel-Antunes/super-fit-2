2e19e8675be5e34b3ee4bb01507dd16e0857e783
Manuel-Antunes <nerdbroz2@gmail.com>
2022-02-17 02:30:00 -0300

    chore: add all media when create exercise

--- app/Http/Controllers/ExercisesController.php
+++ app/Http/Controllers/ExercisesController.php
@@ -2,14 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Exercises;
+use App\Models\Exercise;
 use Illuminate\Http\Request;
 
 class ExercisesController extends Controller
 {
     public function index()
     {
-        $exercises = Exercises::query();
+        $exercises = Exercise::all();
         return view('manage-exercises', ['exercises' => $exercises]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['string', 'required'],
+            'description' => ['string', 'required'],
+        ]);
+        $files = $request->input('media');
+        $exercise = Exercise::create($request->only(['name', 'description']));
+        foreach ($files as $file) {
+            $exercise->media()->attach($file);
+        }
+        return redirect('/exercises');
     }
 }
